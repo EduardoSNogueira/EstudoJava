cc4a5c2a415815df15902cb0e4f8f8e2b419563c
EduardoSNogueira <edu.esn@hotmail.com>
2026-03-08 18:46:03 -0300

    Meu primeiro código Java organizado

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void
+main(String [] args) {
+        System.out.println("Setup Java no Mint: ok!");
+
+        //teste de lógica simples
+        int a = 10;
+        int b = 20;
+        System.out.println("A soma de " + a + " + " + b + " é :" + (a + b));
+
+    }
+
+}
