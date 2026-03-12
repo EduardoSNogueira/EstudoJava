8be02e5e0d249d8fb5416b0294c31c0df58d43a1
EduardoSNogueira <edu.esn@hotmail.com>
2026-03-12 12:13:17 -0300

    feat: adiciona logica para intervalo de numeros ifelse

---
+++ 01-logicaDeProgramacao/10-intervalo-numeros/src/main/java/com/nogueira/Main.java
@@ -0,0 +1,30 @@
+package com.nogueira;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        double valor = sc.nextDouble();
+                
+        if (valor < 0 || valor > 100) {
+            System.out.println("Fora do intervalo");
+        } 
+        else if (valor <= 25) {
+            System.out.println("Intervalo: [0,25]");
+        } 
+        else if (valor <= 50) {
+            System.out.println("Intervalo: (25,50]");
+        } 
+        else if (valor <= 75) {
+            System.out.println("Intervalo: (50,75]");
+        } 
+        else {
+            System.out.println("Intervalo: (75,100]");
+        }
+        sc.close();
+    }
+}
