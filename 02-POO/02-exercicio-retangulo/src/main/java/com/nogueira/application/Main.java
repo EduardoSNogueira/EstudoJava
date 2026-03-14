59ce4bac8a3d97e5f612a48d436f094ed8dd7a6d
EduardoSNogueira <edu.esn@hotmail.com>
2026-03-14 13:59:28 -0300

    feat: implementa classe Rectangle com encapsulamento, construtor e metodos de calculo

---
+++ 02-POO/02-exercicio-retangulo/src/main/java/com/nogueira/application/Main.java
@@ -0,0 +1,22 @@
+package com.nogueira.application;
+
+import com.nogueira.entites.Rectangle;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter rectangle width:  ");
+        double width = sc.nextDouble();
+        System.out.print("Enter rectangle height: ");
+        double height = sc.nextDouble();
+
+        Rectangle rectangle = new Rectangle(width, height);
+        System.out.print(rectangle);
+        sc.close();
+    }
+}
---
+++ 02-POO/02-exercicio-retangulo/src/main/java/com/nogueira/entites/Rectangle.java
@@ -0,0 +1,34 @@
+package com.nogueira.entites;
+
+public class Rectangle {
+
+    // Atributos privados para proteger os dados
+    private double width;
+    private double height;
+
+    // Contrutores
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double area() {
+        return width * height;
+    }
+
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("AREA = %.2f%n", area())
+                + String.format("PERIMETER = %.2f%n", perimeter())
+                + String.format("DIAGONAL = %.2f%n", diagonal());
+    }
+
+}
